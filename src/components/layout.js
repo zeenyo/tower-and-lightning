e5dc63bc4c791896091c3752f4a13fd572818a03
zeenyo <zeno.poopito@gmail.com>
2023-01-09 01:49:33 -0500

    npm -> yarn

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,13 +31,7 @@ const Layout = ({ pageName, children }) => {
         />
       </Helmet>*/}
       <Header menuLinks={data.site.siteMetadata.menuLinks} siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`
-        }}
-      >
+      <div>
         <main id={pageName}>{children}</main>
       </div>
       <Footer />
