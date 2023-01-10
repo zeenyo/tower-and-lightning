dc82cf201836acbbca3e13c483a52a5a0c3509cf
zeenyo <zeno.poopito@gmail.com>
2023-01-09 22:15:47 -0500

    new home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,21 @@ import * as React from "react"
 
 // import Layout from '../components/layout.js'
 
-import Logo from "../images/svgs/tl-logo.inline.svg"
+// import Logo from "../images/svgs/tl-logo.inline.svg"
 import "../styles/style.sass"
 
 const IndexPage = () => {
   return (
-    // <Layout>
       <main>
-        <div class="placeholder">
-          <Logo id="logo" />
-          <h1>Tower & Lightning</h1>
-          <h2>Currently under construction.</h2>
-        </div>
+        <section id="place-hero">
+          <div className="bg"></div>
+          <div className="lightning flashit"></div>
+          <div className="filter">
+            <h1>Tower & Lightning</h1>
+            <p>Coming Soon</p>
+          </div>
+        </section>
       </main>
-    // </Layout>
   )
 }
 
